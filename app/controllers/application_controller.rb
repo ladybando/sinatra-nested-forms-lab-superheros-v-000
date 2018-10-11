412232e183157a43360ec7ac585abe7dc6c0a4ba
Ayana Bando <ayanaflax@yahoo.com>
2018-10-11 16:15:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
     erb :super_hero
   end
   
- post '/team' do
+ post '/teams' do
     @team = Team.new({name: params[:team][:name], motto: params[:team][:motto]})
 
     params[:team][:heroes].each {|hero| Superhero.new(hero)}
